78a9f7862a47b0d6ce006ed012c7e2d2a8f2f5e5
Huangpeiqiang <369645490@qq.com>
2019-05-21 10:25:13 +0800

    del

--- Zerg/application/extra/wx.php
+++ Zerg/application/extra/wx.php
@@ -2,13 +2,9 @@
 /**
  * Created by PhpStorm.
  * User: Administrator
- * Date: 2019/4/6
- * Time: 10:05
+ * Date: 2019/4/3
+ * Time: 20:58
  */
 return [
-    'app_id' => 'wxf01d13c745bed7b8',
-    'app_secret' => 'c3a6b7306d08ea6e85fe62022c7391ab',
-    'login_url' => 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code',
-    //获取access_token的地址
-    'accessGetUrl' => 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=%s&secret=%s'
+
 ];
